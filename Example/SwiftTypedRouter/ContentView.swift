1d67126925d1951dbc7a92222102a90945b0da35
Sam Dean <deanWombourne@gmail.com>
2020-02-03 22:44:04 +0000

    Convert example app to SwiftUI

---
+++ Example/SwiftTypedRouter/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  SwiftTypedRouter_Example
+//
+//  Created by Sam Dean on 03/02/2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("Hello World")
+    }
+}
