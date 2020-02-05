29b0cf61dc9424ed595145313ffbd2a3f2f503d1
Sam Dean <s.dean@lvmhda.com>
2020-02-05 14:38:53 +0000

    Add test coverage, and remove unneeded router setup

--- Example/SwiftTypedRouter/SceneDelegate.swift
+++ Example/SwiftTypedRouter/SceneDelegate.swift
@@ -41,9 +41,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
-        // Create the router _before_ we do anything with a view
-        _ = self.router
-
         // Create the SwiftUI view that provides the window contents.
         let contentView = NavigationView {
             HomeView()
