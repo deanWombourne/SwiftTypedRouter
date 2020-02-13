ee047882920df50aa762ff5d47a7db89ebea7bf2
Sam Dean <s.dean@lvmhda.com>
2020-02-13 13:49:48 +0000

    Explicitly specify spm platform version

--- Package.swift
+++ Package.swift
@@ -3,6 +3,10 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftTypedRouter",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+    ],
     products: [
         .library(name: "SwiftTypedRouter", targets: ["SwiftTypedRouter"]),
     ],
